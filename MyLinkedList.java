7dd4bd1c670876f0d01fb07b63e4fb654a5632c8
renata242 <59351081+renata242@users.noreply.github.com>
2021-04-25 11:26:25 +0600

    Add files via upload

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -17,50 +17,29 @@ public class MyLinkedList<T>{
         }
         size++;
     }
-    public T get (int index){
+     public T get (int index){
         MyNode<T> current = head;
         for(int i=0; i< index; i++){
             current= current.next;
         }
         return current.data;
-    }
+     }
 
-    private static class MyNode<E>{
+     private static class MyNode<E>{
         E data;
         MyNode<E> next;
 
         MyNode(E data){
             this.data=data;
         }
-    }
+     }
     public int getSize() {
         return size;
-    }
-    private MyNode getNode(int index) {
-        if (index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-        MyNode node = head;
-        for (int i=0; i<index; i++) {
-            node = node.next;
-        }
-        return node;
-    }
-
-    public T remove(int index) {
-        T element = get(index);
-        if (index == 0) {
-            head = head.next;
-        } else {
-            MyNode node = getNode(index-1);
-            node.next = node.next.next;
-        }
-        size--;
-        return element;
     }
 
     public static void main(String[] args) {
         MyLinkedList<String> list = new MyLinkedList<>();
+
         list.add("Is");
         list.add("ocean");
         list.add("blue");
@@ -68,11 +47,5 @@ public class MyLinkedList<T>{
         for(int i=0; i< list.getSize(); i++){
             System.out.println(list.get(i)+" ");
         }
-
-        list.remove(1);
-
-        System.out.println(list);
-
     }
 }
-
