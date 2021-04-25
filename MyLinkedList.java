5dcfaa5bbf07a5c9b5478c05b5ff188b0e15ac68
renata242 <59351081+renata242@users.noreply.github.com>
2021-04-25 18:33:35 +0600

    Add files via upload

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,47 +1,74 @@
 package com.company;
+import java.util.Scanner;
+import java.util.Iterator;
 
-public class MyLinkedList<T>{
+public class MyLinkedList<T> implements Iterable<T> {
     private MyNode<T> head;
     private MyNode<T> tail;
     private int size;
 
-    public MyLinkedList(){}
+    public MyLinkedList() {
+    }
 
-    public void add(T newItem){
+    public void add(T newItem) {
         MyNode<T> newNode = new MyNode<>(newItem);
-        if(head == null){
-            head =tail = newNode;
-        } else{
+        if (head == null) {
+            head = tail = newNode;
+        } else {
             tail.next = newNode;
-            tail= newNode;
+            tail = newNode;
         }
         size++;
     }
-    public T get (int index){
+
+    public T get(int index) {
         MyNode<T> current = head;
-        for(int i=0; i< index; i++){
-            current= current.next;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
         }
         return current.data;
     }
 
-    private static class MyNode<E>{
+    private static class MyNode<E> {
         E data;
         MyNode<E> next;
 
-        MyNode(E data){
-            this.data=data;
+        MyNode(E data) {
+            this.data = data;
         }
     }
+
     public int getSize() {
         return size;
     }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<T> {
+        MyNode<T> cursor = tail;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != null;
+        }
+
+        @Override
+        public T next() {
+            T nextItem = cursor.data;
+            cursor = cursor.next;
+            return nextItem;
+        }
+    }
+
     private MyNode getNode(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
         MyNode node = head;
-        for (int i=0; i<index; i++) {
+        for (int i = 0; i < index; i++) {
             node = node.next;
         }
         return node;
@@ -52,7 +79,7 @@ public class MyLinkedList<T>{
         if (index == 0) {
             head = head.next;
         } else {
-            MyNode node = getNode(index-1);
+            MyNode node = getNode(index - 1);
             node.next = node.next.next;
         }
         size--;
@@ -61,29 +88,43 @@ public class MyLinkedList<T>{
 
     public int find(T keyItem) {
         MyNode node = head;
-        for (int i=0; i<size; i++) {
-            if (keyItem==node.data) {
+        for (int i = 0; i < size; i++) {
+            if (keyItem == node.data) {
                 return i;
             }
             node = node.next;
         }
         return -1;
     }
-    
+
     public static void main(String[] args) {
         MyLinkedList<String> list = new MyLinkedList<>();
+
         list.add("Is");
         list.add("ocean");
         list.add("blue");
+//        Scanner my = new Scanner(System.in);
+//        int n= my.nextInt();
+//        for(int i=0;i<n;i++){//add
+//            list.add(i);
+//        }
 
-        for(int i=0; i< list.getSize(); i++){
-            System.out.println(list.get(i)+" ");
+        for (int i = 0; i < list.getSize(); i++) {
+            System.out.println(list.get(i) + " ");
         }
+        System.out.println(list.find("Is"));
 
         list.remove(1);
 
         System.out.println(list);
 
+        for (String item : list) {
+            System.out.println(item + " ");
+        }
+        for(int i=list.getSize()-1;i>=0;i--){
+            System.out.println(list.get(i)+" ");
+        }
     }
-}
+
+    }
 
