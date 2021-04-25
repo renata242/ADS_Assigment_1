e2c6bd409a780ecd148a8d081141eb19be24d065
renata242 <59351081+renata242@users.noreply.github.com>
2021-04-25 19:44:07 +0600

    Add files via upload

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,11 +1,11 @@
 package com.company;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+
+import java.util.Iterator;
 import java.util.Scanner;
 
-public class MyArrayList<T> {
+public class MyArrayList<T> implements Iterable<T>{
     private int size = 0;
-    private int capacity ;
+    private int capacity;
     private Object[] array;
 
     public MyArrayList() {
@@ -32,25 +32,90 @@ public class MyArrayList<T> {
         array = array2;
     }
 
-//    public void print() {
-//        for (int i = 0; i < size; i++) {
-//            System.out.println(array[i]);
-//        }
-//    }
-
-    public int getSize() {
+    public int getSize(){
         return size;
     }
-    public static void main(String[] args){
-        MyArrayList<String> list = new MyArrayList<String>();
 
-        list.add("Is");
-        list.add("ocean");
-        list.add("blue");
+    public int find(T keyItem) {
+        for (int i = 0; i < size; i++) {
+            if (keyItem == array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-        for(int i=0; i< list.getSize(); i++){
-            System.out.println(list.get(i)+" ");
+    public T remove(int index) {
+        T element = get(index);
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+        return element;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIterator();
+    }
+
+    private class MyIterator implements Iterator<T> {
+        int cursor = 0;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != getSize();
+        }
+
+        @Override
+        public T next() {
+            T nextItem = get(cursor);
+            cursor++;
+            return nextItem;
         }
     }
-}
 
+    public static void main(String[] args) {
+//        int[] myArr = { 10,15,47,8,1,-9,0,7 };
+//        Scanner myT = new Scanner(System.in);
+//        System.out.println("choose the number of which index u want to know");
+//        int T = myT.nextInt();
+//        System.out.println("index position of "+ T +" is: "
+//                + find( myArr,T));
+          MyArrayList<Integer> list = new MyArrayList<>();
+
+            list.add(1);
+            list.add(2);
+            list.add(3);
+
+            for (int i = 0; i < list.getSize(); i++) {//simple output
+                System.out.println(list.get(i) + " ");
+            }
+            System.out.println("the index of 1 is:");
+            System.out.println(list.find(1));//find
+
+            list.remove(3);
+            System.out.println("the removed items' index is 3");
+            for (int i = 0; i < list.getSize(); i++) {//simple output
+                System.out.println(list.get(i) + " ");
+            }
+    //
+    //        for (String item : list) {
+    //            System.out.println(item + " ");
+    //        }
+            System.out.println("the reversed order is");
+            Iterator<Integer> st=list.iterator();
+            while (st.hasNext()){
+                System.out.println(st.next());
+            }
+
+//        for(int i=0; i<5; i++){
+//            list.add(i);
+//        }
+//
+//        System.out.println(array.toString(list.toArray()) + " size = " + mal.size);
+//
+//        list.remove(new Integer(2));
+//        System.out.println(array.toString(list.toArray()) + " size = " + mal.size);
+    }
+}
